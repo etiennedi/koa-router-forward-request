5e310a01aa25da2126f4420fb88cef44f4275279
Tomasz Szuba <tomasz.szuba@allegro.pl>
2019-01-21 18:33:48 +0100

    revert

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const doRequest = (method, options) => new Promise((resolve, reject) => {
   })
 });
 
-exports.default = config => async function (ctx) {
+const forward = config => async (ctx) => {
   // parse URL
   const paramKeys = [];
 
@@ -59,4 +59,7 @@ exports.default = config => async function (ctx) {
   // forward status code
   ctx.response.status = res.statusCode;
 };
+
+module.exports = forward;
+
 
